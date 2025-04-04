cec286a509dcf0d2807649f99102ffc703cd4044
bartosztokarz <btokarz@student.agh.edu.pl>
2025-04-04 15:36:04 +0200

    Waves creator

--- Button.py
+++ Button.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 class Button():
-  def __init__(self, x, y, image, click_and_hold):
+  def __init__(self, x, y, image, click_and_hold= False):
     self.image = image
     self.rect = self.image.get_rect()
     self.rect.topleft = (x, y)
